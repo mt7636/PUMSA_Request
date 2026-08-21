d5ad1d73e04d02f0137f0cff85656b8bce26fdca
mt7636 <mt7636@princeton.edu>
2026-08-20 19:11:05 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// PASTE your Apps Script Web App URL here after deploying (see SETUP.md, step 3).
-// It looks like: https://script.google.com/macros/s/AKfycb.../exec
-const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwLLkAZuIoC_GIB7RpHViE6buxkC-y1tahcbyn8JxcwOtrXadgUL0tH21SHBRYP3lXayA/exec';
