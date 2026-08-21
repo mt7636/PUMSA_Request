7dc453d9a865f391c305fd00a4a91550f63b0214
mt7636 <mt7636@princeton.edu>
2026-08-20 19:11:31 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// PASTE your Apps Script Web App URL here after deploying (see SETUP.md, step 3).
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+const BACKEND_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
