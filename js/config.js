96998f256e2e8f00dd6ab1e3180827e2bfe281bb
mt7636 <mt7636@princeton.edu>
2026-08-20 19:13:43 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // PASTE your Apps Script Web App URL here after deploying (see SETUP.md, step 3).
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-const BACKEND_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwLLkAZuIoC_GIB7RpHViE6buxkC-y1tahcbyn8JxcwOtrXadgUL0tH21SHBRYP3lXayA/exec';
